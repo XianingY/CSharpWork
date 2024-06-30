c55634d55e1f33f0eaac101c131713dc7b32b1ad
FanYuan <1569297330@qq.com>
2024-06-30 23:09:20 +0800

    第九次和第十次作业

---
+++ j10sql/Classes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace j10sql
+{
+    public class School
+    {
+        public int SchoolId { get; set; }
+        public string Name { get; set; }
+        public List<Class> Classes { get; set; } = new List<Class>();
+    }
+
+    public class Class
+    {
+        public int ClassId { get; set; }
+        public string Name { get; set; }
+        public int SchoolId { get; set; } // Foreign key to School
+        public List<Student> Students { get; set; } = new List<Student>();
+    }
+
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Grade { get; set; }
+        public int ClassId { get; set; } // Foreign key to Class
+    }
+
+}
